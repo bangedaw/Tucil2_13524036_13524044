380e29a10802774fa2abcc8622ed33dbce987976
raulin <andradaffamarpaung@gmail.com>
2026-03-22 22:40:25 +0700

    feat: main for testing

---
+++ src/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"sync"
+	"time"
+)
+
+func main() {
+	inputFile := "../test/pumpkin.obj"
+	outputFile := "pumpkin-voxelized.obj"
+	maxDepth := 5
+
+	startTime := time.Now()
+
+	vertices, faces, rootBoundary, err := ParseOBJ(inputFile)
+	if err != nil {
+		log.Fatalf("Gagal membaca file .obj: %v", err)
+	}
+
+	rootNode := NewOctreeNode(rootBoundary)
+	stats := NewOctreeStats(maxDepth)
+
+	var wg sync.WaitGroup
+	wg.Add(1)
+
+	BuildOctreeConcurrent(rootNode, vertices, faces, 0, maxDepth, stats, &wg)
+
+	wg.Wait()
+
+	var solidVoxels []BoundingBox
+	CollectVoxels(rootNode, &solidVoxels)
+
+	numVertices, numFaces, err := ExportToObj(solidVoxels, outputFile)
+	if err != nil {
+		log.Fatalf("Gagal menulis file .obj: %v", err)
+	}
+
+	executionTime := time.Since(startTime)
+
+	PrintReport(stats, len(solidVoxels), numVertices, numFaces, maxDepth, executionTime, outputFile)
+
+	fmt.Println("\nMembuka Interactive Viewer...")
+	ViewVoxelsInteractive((solidVoxels))
+}
